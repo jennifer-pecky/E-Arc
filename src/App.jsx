6be7c2037e4947ba349513400af645f3e9290f7b
jennifer-pecky <ugwojennifer7@gmail.com>
2022-11-27 15:49:57 +0100

    E-Arc

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Admin } from './pages';
+
+function App() {
+  return <h1>jjjjjjjj</h1>;
+}
+
+export default App;
---
+++ src/pages/orderHistory/OrderHistory.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './OrderHistory.modukes.scss';
+
+const OrderHistory = () => {
+  return <div>OrderHistory</div>;
+};
+
+export default OrderHistory;
