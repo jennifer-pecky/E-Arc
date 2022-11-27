3ebb9eaafdc7e4e2004d2f83970560393088833a
jennifer-pecky <ugwojennifer7@gmail.com>
2022-11-27 16:39:18 +0100

    worked on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Admin } from './pages';
+import { Admin, Cart, Contact, Home } from './pages';
+import { Footer, Header } from './components';
 
 function App() {
-  return <h1>jjjjjjjj</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<Admin />} />
+          {/* <Route path="*" element={<Footer />} /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
