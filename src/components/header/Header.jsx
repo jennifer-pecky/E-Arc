87951fb25f8715fd4d0df139f741caf74a0a45db
jennifer-pecky <ugwojennifer7@gmail.com>
2022-11-28 13:34:28 +0100

    applied links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+import { FaOpencart } from 'react-icons/fa';
+
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        E- <span>Arc</span>.
+      </h2>
+    </Link>
+  </div>
+);
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className={styles['header-right']}>
+            <span className={styles.links}>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/order-history">My Orders</Link>
+            </span>
+
+            <span className={styles.cart}>
+              <Link to="/cart">
+                Cart
+                <FaOpencart size={20} />
+                <p>0</p>
+              </Link>
+            </span>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
